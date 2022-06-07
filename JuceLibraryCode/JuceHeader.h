953cc729eaf59a956c39bb7e87b88b9908d92720
Tahara Yoshinori <read.eval.print@gmail.com>
2022-06-07 13:44:25 +0900

    juce_audio_formats がないと VoiSona が読み込めなかった

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -14,6 +14,7 @@
 
 
 #include <juce_audio_basics/juce_audio_basics.h>
+#include <juce_audio_formats/juce_audio_formats.h>
 #include <juce_audio_processors/juce_audio_processors.h>
 #include <juce_core/juce_core.h>
 #include <juce_data_structures/juce_data_structures.h>
